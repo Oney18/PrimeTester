e551f096faf52a14b1689463b5c80b95145f35e3
oney18 <oney18@up.edu>
2015-09-03 16:04:38 -0700

    Fixed 1 & 0 issue for good now

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,9 +15,12 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         for(long i = n-1; i > 1; i--){
-            if(n % i == 0 || n == 0 || n == 1){
+            if(n % i == 0){
                 return false;
             }
+        }
+        if( n == 0 || n == 1) {
+            return false;
         }
         return true;
     }
